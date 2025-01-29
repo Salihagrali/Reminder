79a9554991be2cd2662c9c365657476efc3f993b
Salih <imago_999@hotmail.com>
2025-01-30 00:11:16 +0300

    only auth filter left (besides jwt) will test whether it works or not when finished.

--- src/main/java/com/myprojects/reminder/security/SecurityConfig.java
+++ src/main/java/com/myprojects/reminder/security/SecurityConfig.java
@@ -4,52 +4,49 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public EmailAuthenticationFilter emailAuthenticationFilter(){
+        EmailAuthenticationFilter emailAuthenticationFilter = new EmailAuthenticationFilter();
+        emailAuthenticationFilter.setAuthenticationManager(authenticationManager());
+        return emailAuthenticationFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests.anyRequest().authenticated())
+                .addFilterBefore(emailAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
-//    //I'll implement my own auth token, auth provider
-//    @Bean
-//    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) throws Exception {
-////        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-////        authenticationProvider.setUserDetailsService(userDetailsService());
-////        authenticationProvider.setPasswordEncoder(passwordEncoder());
-////
-////        ProviderManager providerManager = new ProviderManager(authenticationProvider);
-////        providerManager.setEraseCredentialsAfterAuthentication(false);
-////
-////        return providerManager;
-//        return new ProviderManager();
-//    }
 
-//    @Bean
-//    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-//        return new InMemoryUserDetailsManager(
-//                User.withUsername("salih").password("1234").roles("USER").build(),
-//                User.withUsername("hanna").password("1234").roles("USER").build()
-//        );
-//    }
-
+    @Bean
+    public AuthenticationManager authenticationManager(){
+          EmailAuthProvider emailAuthProvider = new EmailAuthProvider(userDetailsService, passwordEncoder());
+          return new ProviderManager(emailAuthProvider);
+    }
 }
